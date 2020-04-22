2d15dae781b895f1ae91e5333cfe65a4b9fd0fc4
Luke Karrys <luke@lukekarrys.com>
2020-04-21 22:28:08 -0700

    parse date

--- _plugins/photo_filter.rb
+++ _plugins/photo_filter.rb
@@ -1,14 +1,16 @@
-require "exifr"
+require "date"
 
 module Jekyll
   module PhotoFilter
     def photo_filter(files)
       photos = files.select {|photo| photo.relative_path.include?("original") }
       photos.sort_by do |photo|
-        print `git log --format=%ai "#{photo.path}" | tail -1`
+        date = DateTime.parse(`git log --format=%ai "#{photo.path}" | tail -1`).to_date
+        print photo.path
         print "\n"
-        File.ctime(photo.path)
-        # EXIFR::JPEG.new(photo.path).date_time.to_s
+        print date
+        print "\n\n"
+        date
       end
     end
   end
