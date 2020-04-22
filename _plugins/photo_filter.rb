3b92e4fd4cb709682bbd7a10931afd6c4d1be94e
Luke Karrys <luke@lukekarrys.com>
2020-04-21 21:43:18 -0700

    debug dates

--- _plugins/photo_filter.rb
+++ _plugins/photo_filter.rb
@@ -5,6 +5,11 @@ module Jekyll
     def photo_filter(files)
       photos = files.select {|photo| photo.relative_path.include?("original") }
       photos.sort_by do |photo|
+        print photo.path
+        print "\n"
+        print File.mtime(photo.path)
+        print "\n"
+        print "----\n"
         File.mtime(photo.path)
         # EXIFR::JPEG.new(photo.path).date_time.to_s
       end
