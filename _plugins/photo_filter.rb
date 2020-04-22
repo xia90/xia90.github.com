3eec06ca59a5fc8cc90c1279f406d3ace7465edb
Luke Karrys <luke@lukekarrys.com>
2020-04-21 22:22:38 -0700

    iso

--- _plugins/photo_filter.rb
+++ _plugins/photo_filter.rb
@@ -5,7 +5,7 @@ module Jekyll
     def photo_filter(files)
       photos = files.select {|photo| photo.relative_path.include?("original") }
       photos.sort_by do |photo|
-        print `git log --format=%aD "#{photo.path}" | tail -1`
+        print `git log --format=%ai "#{photo.path}" | tail -1`
         print "\n"
         File.ctime(photo.path)
         # EXIFR::JPEG.new(photo.path).date_time.to_s
